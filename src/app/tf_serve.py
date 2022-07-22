d7f2a6c077b37969d6638dbf5d03f9fd8cd4c0ad
ashmi banerjee <ashmibanerjee1411@gmail.com>
2022-07-22 09:41:56 +0200

    modified curl post

--- src/app/tf_serve.py
+++ src/app/tf_serve.py
@@ -1,12 +1,21 @@
 import json
 import requests
+from src.pred.tf_pred import *
 
 URL = 'http://localhost:9001/v1/models/1657646009:predict'
 
 
-def serve_rest(img):
+def write_file(data, file_name="data.json"):
+    with open(file_name, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+def serve_rest(img_url):
+    img = preprocess_image(img_url)
     data = json.dumps({"signature_name": "serving_default", "instances": img.tolist()})
+    write_file(data)
     headers = {"content-type": "application/json"}
     json_response = requests.post(URL, data=data, headers=headers)
     predictions = json.loads(json_response.text)['predictions']
-    return predictions
+    results = decode_predictions(predictions)
+    return results
--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
-from src.pred.tf_pred import *
+from src.app.tf_serve import *
 from src.utils.test_images import image_links
 
 IMAGE_LINKS = image_links
 
 if __name__ == "__main__":
     for IMAGE_LINK in IMAGE_LINKS:
-        res = tf_predict(IMAGE_LINK["url"], serve_type="REST")
+        res = serve_rest(IMAGE_LINK["url"])
         print(res)
